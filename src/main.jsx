f3fb604ad66e45a45e97324a8737b5803e7ca65d
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-12-17 17:22:31 +0600

    navbar added with basic login registration navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from "./Layout/Root";
+import Home from "./components/Home/Home";
+import './App'
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'login',
+        Component: Login,
+      },
+      {
+        path: 'register',
+        Component: Register,
+      }
+    ]
   },
 ]);
 
